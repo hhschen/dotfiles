33543618d6adff3664d3f02a6a06fad20dfab2bc
Hung Hsun Chen <204702481+hhschen@users.noreply.github.com>
2025-07-08 10:39:29 +0800

    feat(nvim): add useful lsp on_attach function

--- .config/nvim/after/lsp/clangd.lua
+++ .config/nvim/after/lsp/clangd.lua
@@ -1,3 +1,23 @@
+-- https://clangd.llvm.org/extensions.html#switch-between-sourceheader
+local function switch_source_header(bufnr)
+  local method_name = 'textDocument/switchSourceHeader'
+  local client = vim.lsp.get_clients({ bufnr = bufnr, name = 'clangd' })[1]
+  if not client then
+    return vim.notify(('method %s is not supported by any servers active on the current buffer'):format(method_name))
+  end
+  local params = vim.lsp.util.make_text_document_params(bufnr)
+  client.request(method_name, params, function(err, result)
+    if err then
+      error(tostring(err))
+    end
+    if not result then
+      vim.notify('corresponding file cannot be determined')
+      return
+    end
+    vim.cmd.edit(vim.uri_to_fname(result))
+  end, bufnr)
+end
+
 ---@type vim.lsp.Config
 return {
   capabilities = {
@@ -32,4 +52,9 @@ return {
     'configure.ac', -- AutoTools
     '.git',
   },
+  on_attach = function(_, bufnr)
+    vim.api.nvim_buf_create_user_command(bufnr, 'LspClangdSwitchSourceHeader', function()
+      switch_source_header(bufnr)
+    end, { desc = 'Switch between source/header' })
+  end,
 }
